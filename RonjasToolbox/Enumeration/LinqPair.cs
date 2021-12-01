5337de5810f2f014cad82ed2812f8a76fd8fccff
Ronja <git@ronja.space>
2021-12-01 12:47:43 +0100

    Add enumerator pairs and triplets

---
+++ RonjasToolbox/Enumeration/LinqPair.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+
+namespace RonjasToolbox; 
+
+public static class LinqPair {
+	public static IEnumerable<(T First, T Second)> Pair<T>(this IEnumerable<T> input) {
+		return new PairEnumerable<T>(input);
+	}
+
+	private class PairEnumerable<T> : IEnumerable<(T First, T Second)> {
+		private readonly IEnumerable<T> source;
+		public PairEnumerable(IEnumerable<T> input) {
+			source = input;
+		}
+
+		public IEnumerator<(T First, T Second)> GetEnumerator() {
+			return new PairEnumerator<T>(source.GetEnumerator());
+		}
+
+		IEnumerator IEnumerable.GetEnumerator() {
+			return GetEnumerator();
+		}
+	}
+
+	private class PairEnumerator<T> : IEnumerator<(T First, T Second)> {
+		private T? previous = default;
+		private readonly IEnumerator<T> source;
+		public PairEnumerator(IEnumerator<T> input) {
+			source = input;
+			Reset();
+		}
+
+		public bool MoveNext() {
+			previous = source.Current;
+			return source.MoveNext();
+		}
+
+		public void Reset() {
+			try {
+				source.Reset();
+			} catch(NotImplementedException) {}
+			source.MoveNext();
+		}
+
+		public (T First, T Second) Current => (previous!, source.Current);
+
+		object IEnumerator.Current => Current;
+
+		public void Dispose() {
+			source.Dispose();
+		}
+	}
+}
---
+++ RonjasToolbox/Enumeration/LinqTriplet.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+
+namespace RonjasToolbox; 
+
+public static class LinqTriplet {
+	public static IEnumerable<(T First, T Second, T Third)> Triplet<T>(this IEnumerable<T> input) {
+		return new TripletEnumerable<T>(input);
+	}
+
+	private class TripletEnumerable<T> : IEnumerable<(T First, T Second, T Third)> {
+		private readonly IEnumerable<T> source;
+		public TripletEnumerable(IEnumerable<T> input) {
+			source = input;
+		}
+
+		public IEnumerator<(T First, T Second, T Third)> GetEnumerator() {
+			return new TripletEnumerator<T>(source.GetEnumerator());
+		}
+
+		IEnumerator IEnumerable.GetEnumerator() {
+			return GetEnumerator();
+		}
+	}
+
+	private class TripletEnumerator<T> : IEnumerator<(T First, T Second, T Third)> {
+		private T? previous = default;
+		private T? previous2 = default;
+		private readonly IEnumerator<T> source;
+		public TripletEnumerator(IEnumerator<T> input) {
+			source = input;
+			Reset();
+		}
+
+		public bool MoveNext() {
+			previous2 = previous;
+			previous = source.Current;
+			return source.MoveNext();
+		}
+
+		public void Reset() {
+			try {
+				source.Reset();
+			} catch(NotImplementedException) {}
+			source.MoveNext(); //start source enumerator
+			MoveNext();
+		}
+
+		public (T First, T Second, T Third) Current => (previous2!, previous!, source.Current);
+
+		object IEnumerator.Current => Current;
+
+		public void Dispose() {
+			source.Dispose();
+		}
+	}
+}
